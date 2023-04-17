b044545ebeed4581bbdcb74a0d1b72ad54ba4edb
snhkn <senihaakin@hotmail.com>
2023-04-17 14:57:28 +0300

    Initial Commit

---
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/TodoAppWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.snhkn.rest.webservices.todoAppwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/snhkn/rest/webservices/todoAppwebservice/TodoAppWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.snhkn.rest.webservices.todoAppwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
