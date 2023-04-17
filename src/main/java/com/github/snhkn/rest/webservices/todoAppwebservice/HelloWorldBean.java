c3b96cd67db6123053a1782c3a13e4d87a32b1e5
snhkn <senihaakin@hotmail.com>
2023-04-17 15:26:02 +0300

    Enhance service to return a bean

---
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.github.snhkn.rest.webservices.todoAppwebservice;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	// having a getter method is important
+	// if you don't have it, you will not able to display the bean!
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+}
--- src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/HelloWorldController.java
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/HelloWorldController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @RestController
 public class HelloWorldController {
-	// GET
-	// URI - /hello-world
-	// method - "Hello World"
+
 	@GetMapping(path = "/hello-world")
 	public String HelloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean HelloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
