81fd2022af322eba56b890fa0fafccd2155f4865
snhkn <senihaakin@hotmail.com>
2023-04-18 15:29:29 +0300

    Add TodoResource and related classes for retrieving Todos by username

---
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/todo/TodoHardcodedService.java
@@ -0,0 +1,24 @@
+package com.github.snhkn.rest.webservices.todoAppwebservice.todo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardcodedService {
+
+	private static List<Todo> todos = new ArrayList();
+	private static int idCounter = 0;
+
+	static {
+		todos.add(new Todo(++idCounter, "defaultuser", "Learn Angular", new Date(), false));
+		todos.add(new Todo(++idCounter, "defaultuser", "Clean the house", new Date(), false));
+		todos.add(new Todo(++idCounter, "defaultuser", "Study German", new Date(), false));
+	}
+
+	public List<Todo> findAll() {
+		return todos;
+	}
+}
