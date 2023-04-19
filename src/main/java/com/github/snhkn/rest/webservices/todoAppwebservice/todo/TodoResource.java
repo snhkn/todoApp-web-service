db0ceae9cb5c582e3ee51f83d24164faecfbbdbe
snhkn <senihaakin@hotmail.com>
2023-04-19 13:07:29 +0300

    feat: Create REST API for updating todo

--- src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/todo/TodoResource.java
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/todo/TodoResource.java
@@ -3,11 +3,14 @@ package com.github.snhkn.rest.webservices.todoAppwebservice.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -34,6 +37,13 @@ public class TodoResource {
 	@GetMapping("/users/{username}/todos/{id}")
 	public Todo getTodo(@PathVariable String username, @PathVariable long id) {
 		return todoService.findById(id);
+	}
+
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id,
+			@RequestBody Todo todo) {
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
 	}
 
 }
