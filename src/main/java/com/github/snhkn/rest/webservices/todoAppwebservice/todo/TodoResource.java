f369e6fae365a6e8cf00145934d1145333836e8c
snhkn <senihaakin@hotmail.com>
2023-04-19 14:18:48 +0300

    feat: Create service to create a new Todo

--- src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/todo/TodoResource.java
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/todo/TodoResource.java
@@ -1,5 +1,6 @@
 package com.github.snhkn.rest.webservices.todoAppwebservice.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -44,6 +47,20 @@ public class TodoResource {
 			@RequestBody Todo todo) {
 		Todo todoUpdated = todoService.save(todo);
 		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+	}
+
+	// Create a new Todo
+	// POST /users/{user_name}/todos
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
+		Todo createdTodo = todoService.save(todo);
+		// Location
+		// Get current resource url
+		// {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 }
