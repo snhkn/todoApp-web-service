dedbf33ade1c07a88fde40df7f805b44b5d564c6
snhkn <senihaakin@hotmail.com>
2023-04-20 20:51:43 +0300

    feat: Create Basic Authentication RESTful service

---
+++ src/main/java/com/github/snhkn/rest/webservices/todoAppwebservice/basic/auth/AuthenticationBean.java
@@ -0,0 +1,18 @@
+package com.github.snhkn.rest.webservices.todoAppwebservice.basic.auth;
+
+public class AuthenticationBean {
+	private String message;
+	public AuthenticationBean(String message) {
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+}
