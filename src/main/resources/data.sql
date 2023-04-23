62becd2a4cb0a0fcc0726f541f551b34a84d04a8
snhkn <senihaakin@hotmail.com>
2023-04-23 22:48:35 +0300

    feat: Set up Todo Entity and populate some Todo records in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001,'someuser','Learn JPA', now(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002,'defaultuser','Learn Microservices', now(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003,'defaultuser','Respond to emails', now(), false);
